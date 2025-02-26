0254f179d429cd19fda89a4f7d3b9205096c11a3
M Possible <185700337+MTS-sip@users.noreply.github.com>
2025-02-25 20:30:56 -0500

    temp removal of env file client

--- server/src/config/connections.js
+++ server/src/config/connections.js
@@ -1,17 +1,16 @@
-const { Sequelize } = require("sequelize");
+import { Sequelize } from "sequelize";
 
-let sequelize;
-// If the environment variable DB_URL is set, use it to connect to the database.
-if (process.env.DB_URL) {
-  sequelize = new Sequelize(process.env.DB_URL);
-} else {
-  sequelize = new Sequelize(
-    process.env.DB_NAME || "default_db_name",
-    process.env.DB_USER || "default_db_user",
-    process.env.DB_PW || "default_db_pw",
-    { host: "localhost", dialect: "postgres" }
-  );
-}
+const sequelize = process.env.DB_URL
+  ? new Sequelize(process.env.DB_URL)
+  : new Sequelize(
+      process.env.DB_NAME || "default_db_name",
+      process.env.DB_USER || "default_db_user",
+      process.env.DB_PASSWORD || "default_db_pw",
+      {
+        host: process.env.DB_HOST || "localhost",
+        dialect: "postgres",
+      }
+    );
 
-module.exports = sequelize;
-// Export the connection
+//  ES module export
+export default sequelize;
